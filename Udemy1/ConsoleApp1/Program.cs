16f455b705bc1cc7c3b60e82e4eb4fa2d22a88b8
diboti <korposb@gmail.com>
2025-06-07 20:29:35 +0300

    calc

--- Udemy1/ConsoleApp1/Program.cs
+++ Udemy1/ConsoleApp1/Program.cs
@@ -1,4 +1,20 @@
-﻿Console.WriteLine("Enter something");
-string input = Console.ReadLine();
-Console.WriteLine("You entered " + input);
+﻿Console.WriteLine("Enter a number!");
+
+int number1 = 0;
+
+string userInput = Console.ReadLine();
+number1 = int.Parse(userInput);
+
+
+Console.WriteLine("Enter a second number!");
+
+int number2 = 0;
+
+userInput = Console.ReadLine();
+number2 = int.Parse(userInput);
+
+int sum = number1 + number2;
+
+Console.WriteLine("The sum of them is: " + sum);
+
 Console.ReadKey();
