1286a4ba4acbfccff592da152164ae128c1bbd96
diboti <korposb@gmail.com>
2025-06-08 12:51:42 +0300

    string manipulation

--- Udemy1/ConsoleApp1/Program.cs
+++ Udemy1/ConsoleApp1/Program.cs
@@ -15,12 +15,31 @@ double number2 = 0;
 userInput = Console.ReadLine();
 number2 = double.Parse(userInput);
 
-double sum = number1 + number2;
-
 // concat
 // Console.WriteLine("The sum of them is: " + sum);
 
 // interp
-Console.WriteLine($"The sum of them is {sum}");
+Console.WriteLine($"The sum of them is {number1 + number2}");
+
+Console.WriteLine($"The sub of them is {number1 - number2}");
+
+Console.WriteLine($"The mul of them is {number1 * number2}");
+
+Console.WriteLine($"The div of them is {number1 / number2}");
 
 Console.ReadKey();
+
+// string formatting
+
+int num = 10;
+double price = 19.75;
+string name = "Frank";
+
+// interpolation
+Console.WriteLine($"The number is {num}");
+
+// concat
+Console.WriteLine("The number is" + num);
+
+// formatting
+Console.WriteLine("The number is {0}, price is {1}, name is {2}", num, price, name);
