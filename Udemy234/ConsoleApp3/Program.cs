5007d3350fa90c391fa877a5fad9f7b41a8fe429
diboti <korposb@gmail.com>
2025-06-08 16:21:34 +0300

    old template

---
+++ Udemy234/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp3;
+
+class Program
+{
+    static int myResult;
+
+    static void Main(string[] args)
+    {
+        int num1 = 1;
+        myResult = AddTwoValues(num1, 10);
+
+        Console.WriteLine(myResult);
+    }
+
+    static int AddTwoValues(int value1, int value2)
+    {
+        return value1 + value2;
+    }
+
+    static int SubtractTwoValues(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+
+}
+
