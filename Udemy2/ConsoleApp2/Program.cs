c09ff4e139151136ed1f3be155ceae50528e1650
diboti <korposb@gmail.com>
2025-06-08 14:21:59 +0300

    2-3 sections done

--- Udemy2/ConsoleApp2/Program.cs
+++ Udemy2/ConsoleApp2/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// bool isRainy = true;
+// bool hasUmbrella = true;
+
+// if (isRainy && hasUmbrella)
+// {
+//     Console.WriteLine("It's rainyyyy and it's ok");
+// }
+
+// switch (isRainy)
+// {
+//     case true:
+//         Console.WriteLine("rains");
+//         break;
+//     case false:
+//         Console.WriteLine("hell nah");
+//         break;
+//     default:
+//         Console.WriteLine("NOO");
+//         break;
+
+// }
+
+
+// // try parse
+// string inputString = Console.ReadLine();
+// int num = 0;
+// if (!int.TryParse(inputString, out num))
+// {
+//     Console.WriteLine("Not a number");
+// }
+// num++;
+// Console.WriteLine(num);
+
+
+// // random number
+// Random random = new Random();
+// int randomNumber = random.Next(1, 11);
+// Console.WriteLine(randomNumber);
+
+string rocket = " |^|\n | |\n | |\n MMM";
+Console.WriteLine(rocket);
+
+for (int i = 0; i < 20; ++i)
+{
+    Console.WriteLine(rocket);
+    for (int j = 20; j > i; --j)
+    {
+        Console.WriteLine("\n");
+    }
+    Thread.Sleep(1000);
+    Console.Clear();
+}
+
+Console.WriteLine("Rocket Landed!");
